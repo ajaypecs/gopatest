0aa75f3eeb9302cbbeb52f07d5ee2b5781312bd8
Ajay <ajay.sharma7707@gmail.com>
2021-07-29 12:20:59 +0530

    changed and create fill products table with faker seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+        foreach (range(1,100) as $value) {
+        	DB::table('products')->insert
+        	([
+        	'name'=> $faker->name(),
+        	'created_at' => now()->format('Y-m-d H:i:s'),
+    		'updated_at' => now()->format('Y-m-d H:i:s'),
+        	]);
+        }
     }
 }
